32de927d91737cccb6e16e4aaded40a6b9416132
Pilgrim132333333 <18649201011@163.com>
2026-03-11 10:46:26 +0800

    config and database 配置更新

--- fastapi-backend/app/database.py
+++ fastapi-backend/app/database.py
@@ -1,1 +1,34 @@
 # 数据库连接、Session、init_db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+from typing import Generator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create the asynchronous engine
+engine = create_engine(
+    settings.DATABASE_URL, 
+    echo=True, 
+    future=True,
+    pool_pre_ping=True
+)
+
+sessionmaker = sessionmaker(
+    autocommit = False,
+    autoflush= False,
+    bind = engine
+)
+
+Base = declarative_base()
+
+def get_db() -> Generator:
+    db = sessionmaker()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
